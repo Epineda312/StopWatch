47559ccabab05afd4761e382511bb5e857a44b3b
Air-ic <epineda2@neiu.edu>
2017-06-16 17:48:01 -0500

    Add files via upload

---
+++ Stopwatch/Stopwatch/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Stopwatch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblMin = new System.Windows.Forms.Label();
+            this.lblSec = new System.Windows.Forms.Label();
+            this.lblMilSec = new System.Windows.Forms.Label();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.stopBtn = new System.Windows.Forms.Button();
+            this.restartBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblMin
+            // 
+            this.lblMin.AutoSize = true;
+            this.lblMin.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMin.Location = new System.Drawing.Point(83, 73);
+            this.lblMin.Name = "lblMin";
+            this.lblMin.Size = new System.Drawing.Size(64, 46);
+            this.lblMin.TabIndex = 0;
+            this.lblMin.Text = "00";
+            // 
+            // lblSec
+            // 
+            this.lblSec.AutoSize = true;
+            this.lblSec.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSec.Location = new System.Drawing.Point(163, 73);
+            this.lblSec.Name = "lblSec";
+            this.lblSec.Size = new System.Drawing.Size(64, 46);
+            this.lblSec.TabIndex = 1;
+            this.lblSec.Text = "00";
+            this.lblSec.Click += new System.EventHandler(this.lblSec_Click);
+            // 
+            // lblMilSec
+            // 
+            this.lblMilSec.AutoSize = true;
+            this.lblMilSec.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMilSec.Location = new System.Drawing.Point(242, 90);
+            this.lblMilSec.Name = "lblMilSec";
+            this.lblMilSec.Size = new System.Drawing.Size(34, 25);
+            this.lblMilSec.TabIndex = 2;
+            this.lblMilSec.Text = "00";
+            this.lblMilSec.Click += new System.EventHandler(this.lblMilSec_Click);
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(61, 147);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(85, 34);
+            this.startBtn.TabIndex = 3;
+            this.startBtn.Text = "Start";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // stopBtn
+            // 
+            this.stopBtn.Location = new System.Drawing.Point(152, 147);
+            this.stopBtn.Name = "stopBtn";
+            this.stopBtn.Size = new System.Drawing.Size(75, 34);
+            this.stopBtn.TabIndex = 4;
+            this.stopBtn.Text = "Stop";
+            this.stopBtn.UseVisualStyleBackColor = true;
+            this.stopBtn.Click += new System.EventHandler(this.stopBtn_Click);
+            // 
+            // restartBtn
+            // 
+            this.restartBtn.Location = new System.Drawing.Point(233, 147);
+            this.restartBtn.Name = "restartBtn";
+            this.restartBtn.Size = new System.Drawing.Size(81, 34);
+            this.restartBtn.TabIndex = 5;
+            this.restartBtn.Text = "Restart";
+            this.restartBtn.UseVisualStyleBackColor = true;
+            this.restartBtn.Click += new System.EventHandler(this.restartBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(142, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 46);
+            this.label1.TabIndex = 6;
+            this.label1.Text = ":";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(219, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 25);
+            this.label2.TabIndex = 7;
+            this.label2.Text = ".";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(354, 314);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.restartBtn);
+            this.Controls.Add(this.stopBtn);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.lblMilSec);
+            this.Controls.Add(this.lblSec);
+            this.Controls.Add(this.lblMin);
+            this.Name = "Form1";
+            this.Text = "Eric\'s Beast StopWatch";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMin;
+        private System.Windows.Forms.Label lblSec;
+        private System.Windows.Forms.Label lblMilSec;
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.Button stopBtn;
+        private System.Windows.Forms.Button restartBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Stopwatch/Stopwatch/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stopwatch
+{
+    public partial class Form1 : Form
+    {
+        int timeCs, timeSec, timeMin;
+        bool isActive;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void lblSec_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void lblMilSec_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            isActive = true;
+        }
+        private void stopBtn_Click(object sender, EventArgs e)
+        {
+            isActive = false;
+        }
+
+        private void restartBtn_Click(object sender, EventArgs e)
+        {
+            isActive = false;
+
+            ResetTime();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (isActive)
+            {
+                timeCs++;
+
+                if (timeCs >= 100)
+                {
+                    timeSec++;
+                    timeCs = 0;
+
+                    if (timeSec >= 60)
+                    {
+                        timeMin++;
+                        timeSec = 0;
+                    }
+                }
+            }
+            DrawTime();
+        }
+
+        private void DrawTime()
+        {
+            lblMilSec.Text = String.Format("{0:00}", timeCs);
+            lblSec.Text = String.Format("{0:00}", timeSec);
+            lblMin.Text = String.Format("{0:00}", timeMin);
+        }
+
+        
+
+        private void ResetTime()
+        {
+            timeCs = 0;
+            timeSec = 0;
+            timeMin = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ResetTime();
+
+            isActive = false;
+        }
+    }
+}
